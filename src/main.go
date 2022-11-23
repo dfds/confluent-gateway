7590bf17b3c89bce2bb7321ab3e82b2dc22fd636
Jens Andresen <jandr@DFDS.COM>
2022-11-23 07:55:31 +0100

    add logic outline to main module

--- src/main.go
+++ src/main.go
@@ -16,3 +16,10 @@ func main() {
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
+
+//1. Ensure capability has service account
+//2. Ensure service account has all acls
+//3. Ensure service account has api keys
+//4. Ensure api keys are stored in vault
+//5. Ensure topic is created
+//6. Done!
