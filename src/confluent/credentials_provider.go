b01cd9f7a3b84c05909f53416c33cd9d27caa8a5
Jens Andresen <jandr@DFDS.COM>
2022-11-25 09:27:13 +0100

    rename client.go to confluent-client.go and removed unused file

--- src/confluent/credentials_provider.go
+++
@@ -1,8 +0,0 @@
-package confluent
-
-//import "github.com/dfds/confluent-gateway"
-//
-//type CredentialsProvider interface {
-//	GetForAccount() string
-//	GetForCluster(id main.ClusterId) string
-//}
