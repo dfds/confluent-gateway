6d58e535c91b5845473e955baec7aa4ff03200b6
Thomas Wittrup Fischer <thofisch@gmail.com>
2022-11-29 23:29:13 +0100

    conform database to confluent's access model

--- db/migrations/initial_ddl.sql
+++ db/migrations/initial_ddl.sql
@@ -12,12 +12,37 @@ CREATE TABLE cluster (
 CREATE TABLE service_account (
     id varchar(255) NOT NULL,
     capability_root_id varchar(255) NOT NULL,
-    cluster_id varchar(255) NOT NULL,
     api_key_username varchar(255) NULL,
     api_key_password varchar(255) NULL,
     created_at timestamp NOT NULL,
 
     CONSTRAINT service_account_pk PRIMARY KEY(id)
+);
+
+CREATE TABLE cluster_access (
+    id uuid NOT NULL,
+    service_account_id varchar(255) NULL,
+    cluster_id varchar(255) NOT NULL,
+    api_key_username varchar(255) NULL,
+    api_key_password varchar(255) NULL,
+    created_at timestamp NOT NULL,
+
+    CONSTRAINT cluster_access_pk PRIMARY KEY(id),
+    CONSTRAINT fk_service_account FOREIGN KEY(service_account_id) REFERENCES service_account(id)
+);
+
+CREATE TABLE acl (
+    id uuid NOT NULL,
+    cluster_access_id uuid NOT NULL,
+	resource_type varchar(255) NOT NULL,
+	resource_name varchar(255) NOT NULL,
+	pattern_type varchar(255) NOT NULL,
+	operation_type varchar(255) NOT NULL,
+	permission_type varchar(255) NOT NULL,
+    created_at timestamp NULL,
+
+    CONSTRAINT acl_pk PRIMARY KEY(id),
+    CONSTRAINT fk_cluster_access FOREIGN KEY(cluster_access_id) REFERENCES cluster_access(id)
 );
 
 
@@ -28,27 +53,13 @@ CREATE TABLE process (
     topic_name varchar(255) NOT NULL,
     topic_partitions int NOT NULL,
     topic_retention int NOT NULL,
-    service_account_id varchar(255) NULL,
-    api_key_username varchar(255) NULL,
-    api_key_password varchar(255) NULL,
-    api_key_created_at timestamp NULL,
+    has_service_account boolean NOT NULL DEFAULT(false),
+    has_cluster_access boolean NOT NULL DEFAULT(false),
+    has_api_key boolean NOT NULL DEFAULT(false),
+    has_api_key_in_vault boolean NOT NULL DEFAULT(false),
     created_at timestamp NULL,
     completed_at timestamp NULL,
 
-    CONSTRAINT process_pk PRIMARY KEY(id),
-    CONSTRAINT fk_service_account FOREIGN KEY(service_account_id) REFERENCES service_account(id)
+    CONSTRAINT process_pk PRIMARY KEY(id)
 );
 
-CREATE TABLE acl (
-    id uuid NOT NULL,
-    process_id uuid NOT NULL,
-	resource_type varchar(255) NOT NULL,
-	resource_name varchar(255) NOT NULL,
-	pattern_type varchar(255) NOT NULL,
-	operation_type varchar(255) NOT NULL,
-	permission_type varchar(255) NOT NULL,
-    created_at timestamp NULL,
-
-    CONSTRAINT acl_pk PRIMARY KEY(id),
-    CONSTRAINT fk_process FOREIGN KEY(process_id) REFERENCES process(id)
-);
