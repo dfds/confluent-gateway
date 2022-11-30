722676914dc24ce3150efeec49e786d4d341dd61
Thomas Wittrup Fischer <thofisch@gmail.com>
2022-11-30 10:27:12 +0100

    remove unused methods

--- src/database/dataaccess.go
+++ src/database/dataaccess.go
@@ -11,20 +11,6 @@ type dataAccess struct {
 	db *gorm.DB
 }
 
-func (da *dataAccess) Transaction(f func(models.DataAccess) error) error {
-	return da.db.Debug().Transaction(func(tx *gorm.DB) error {
-		return f(&dataAccess{tx})
-	})
-}
-
-func (da *dataAccess) ServiceAccounts() models.ServiceAccountRepository {
-	return NewServiceAccountRepository(da.db)
-}
-
-func (da *dataAccess) Processes() models.ProcessRepository {
-	return NewProcessRepository(da.db)
-}
-
 func (da *dataAccess) NewSession(ctx context.Context) models.DataSession {
 	return &dataSession{da.db.Session(&gorm.Session{Context: ctx})}
 }
