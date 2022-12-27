c1f0dff7fb22f2440fe03b7b959a493fce87d0d4
Thomas Wittrup Fischer <thofisch@gmail.com>
2022-12-27 19:36:51 +0100

    move steps close to usage

--- src/process/create_topic.go
+++ src/process/create_topic.go
@@ -2,6 +2,7 @@ package process
 
 import (
 	"context"
+	"fmt"
 	"github.com/dfds/confluent-gateway/models"
 )
 
@@ -87,3 +88,121 @@ func getOrCreateProcessState(repository stateRepository, capabilityRootId models
 	}
 	return state, nil
 }
+
+// region Steps
+
+func ensureServiceAccount(process *Process) error {
+	capabilityRootId := process.State.CapabilityRootId
+	clusterId := process.State.ClusterId
+	service := process.service()
+
+	fmt.Println("### EnsureServiceAccount")
+
+	if process.State.HasServiceAccount {
+		return nil
+	}
+
+	err := service.CreateServiceAccount(capabilityRootId, clusterId)
+	if err != nil {
+		return err
+	}
+
+	process.State.HasServiceAccount = true
+
+	return err
+}
+
+func ensureServiceAccountAcl(process *Process) error {
+	service := process.service()
+	capabilityRootId := process.State.CapabilityRootId
+	clusterId := process.State.ClusterId
+
+	fmt.Println("### EnsureServiceAccountAcl")
+	if process.State.HasClusterAccess {
+		return nil
+	}
+
+	clusterAccess, err := service.GetOrCreateClusterAccess(capabilityRootId, clusterId)
+	if err != nil {
+		return err
+	}
+
+	entries := clusterAccess.GetAclPendingCreation()
+	if len(entries) == 0 {
+		// no acl entries left => mark as done
+		process.State.HasClusterAccess = true
+		return nil
+
+	} else {
+		nextEntry := entries[0]
+
+		return service.CreateAclEntry(clusterId, clusterAccess.ServiceAccountId, &nextEntry)
+	}
+}
+
+func ensureServiceAccountApiKey(process *Process) error {
+	service := process.service()
+	capabilityRootId := process.State.CapabilityRootId
+	clusterId := process.State.ClusterId
+
+	fmt.Println("### EnsureServiceAccountApiKey")
+	if process.State.HasApiKey {
+		return nil
+	}
+
+	clusterAccess, err := service.GetClusterAccess(capabilityRootId, clusterId)
+	if err != nil {
+		return err
+	}
+
+	err2 := service.CreateApiKey(clusterAccess)
+	if err2 != nil {
+		return err2
+	}
+
+	process.State.HasApiKey = true
+	return nil
+}
+
+func ensureServiceAccountApiKeyAreStoredInVault(process *Process) error {
+	service := process.service()
+	aws := process.Vault
+	capabilityRootId := process.State.CapabilityRootId
+	clusterId := process.State.ClusterId
+
+	fmt.Println("### EnsureServiceAccountApiKeyAreStoredInVault")
+	if process.State.HasApiKeyInVault {
+		return nil
+	}
+
+	clusterAccess, err := service.GetClusterAccess(capabilityRootId, clusterId)
+	if err != nil {
+		return err
+	}
+
+	if err := aws.StoreApiKey(context.TODO(), capabilityRootId, clusterAccess.ClusterId, clusterAccess.ApiKey); err != nil {
+		return err
+	}
+
+	process.State.HasApiKeyInVault = true
+
+	return nil
+}
+
+func ensureTopicIsCreated(process *Process) error {
+	fmt.Println("### EnsureTopicIsCreated")
+	if process.State.IsCompleted() {
+		return nil
+	}
+
+	err := process.createTopic()
+	if err != nil {
+		return err
+	}
+
+	process.State.MarkAsCompleted()
+
+	return nil
+}
+
+// endregion
