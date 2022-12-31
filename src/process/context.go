d8dfcccb3e7fdb111aa4f26aa427f3a51343860a
Thomas Wittrup Fischer <thofisch@gmail.com>
2022-12-31 11:40:11 +0100

    don't expose private parts

--- src/process/context.go
+++ src/process/context.go
@@ -5,15 +5,15 @@ import (
 )
 
 type StepContext struct {
-	State   *models.ProcessState
-	Account AccountService
-	Vault   VaultService
-	Topic   TopicService
-	Outbox  Outbox
+	state   *models.ProcessState
+	account AccountService
+	vault   VaultService
+	topic   TopicService
+	outbox  Outbox
 }
 
 func NewStepContext(state *models.ProcessState, account AccountService, vault VaultService, topic TopicService, outbox Outbox) *StepContext {
-	return &StepContext{State: state, Account: account, Vault: vault, Topic: topic, Outbox: outbox}
+	return &StepContext{state: state, account: account, vault: vault, topic: topic, outbox: outbox}
 }
 
 type AccountService interface {
@@ -37,78 +37,78 @@ type Outbox interface {
 }
 
 func (p *StepContext) MarkServiceAccountAsReady() {
-	p.State.HasServiceAccount = true
+	p.state.HasServiceAccount = true
 }
 
 func (p *StepContext) CreateServiceAccount() error {
-	return p.Account.CreateServiceAccount(p.State.CapabilityRootId, p.State.ClusterId)
+	return p.account.CreateServiceAccount(p.state.CapabilityRootId, p.state.ClusterId)
 }
 
 func (p *StepContext) HasServiceAccount() bool {
-	return p.State.HasServiceAccount
+	return p.state.HasServiceAccount
 }
 
 func (p *StepContext) HasClusterAccess() bool {
-	return p.State.HasClusterAccess
+	return p.state.HasClusterAccess
 }
 
 func (p *StepContext) GetOrCreateClusterAccess() (*models.ClusterAccess, error) {
-	return p.Account.GetOrCreateClusterAccess(p.State.CapabilityRootId, p.State.ClusterId)
+	return p.account.GetOrCreateClusterAccess(p.state.CapabilityRootId, p.state.ClusterId)
 }
 
 func (p *StepContext) CreateAclEntry(clusterAccess *models.ClusterAccess, nextEntry models.AclEntry) error {
-	return p.Account.CreateAclEntry(p.State.ClusterId, clusterAccess.ServiceAccountId, &nextEntry)
+	return p.account.CreateAclEntry(p.state.ClusterId, clusterAccess.ServiceAccountId, &nextEntry)
 }
 
 func (p *StepContext) MarkClusterAccessAsReady() {
-	p.State.HasClusterAccess = true
+	p.state.HasClusterAccess = true
 }
 
 func (p *StepContext) HasApiKey() bool {
-	return p.State.HasApiKey
+	return p.state.HasApiKey
 }
 
 func (p *StepContext) GetClusterAccess() (*models.ClusterAccess, error) {
-	return p.Account.GetClusterAccess(p.State.CapabilityRootId, p.State.ClusterId)
+	return p.account.GetClusterAccess(p.state.CapabilityRootId, p.state.ClusterId)
 }
 
 func (p *StepContext) CreateApiKey(clusterAccess *models.ClusterAccess) error {
-	return p.Account.CreateApiKey(clusterAccess)
+	return p.account.CreateApiKey(clusterAccess)
 }
 
 func (p *StepContext) MarkApiKeyAsReady() {
-	p.State.HasApiKey = true
+	p.state.HasApiKey = true
 }
 
 func (p *StepContext) HasApiKeyInVault() bool {
-	return p.State.HasApiKeyInVault
+	return p.state.HasApiKeyInVault
 }
 
 func (p *StepContext) StoreApiKey(clusterAccess *models.ClusterAccess) error {
-	return p.Vault.StoreApiKey(p.State.CapabilityRootId, clusterAccess)
+	return p.vault.StoreApiKey(p.state.CapabilityRootId, clusterAccess)
 }
 
 func (p *StepContext) MarkApiKeyInVaultAsReady() {
-	p.State.HasApiKeyInVault = true
+	p.state.HasApiKeyInVault = true
 }
 
 func (p *StepContext) IsCompleted() bool {
-	return p.State.IsCompleted()
+	return p.state.IsCompleted()
 }
 
 func (p *StepContext) CreateTopic() error {
-	return p.Topic.CreateTopic(p.State.ClusterId, p.State.Topic())
+	return p.topic.CreateTopic(p.state.ClusterId, p.state.Topic())
 }
 
 func (p *StepContext) MarkAsCompleted() {
-	p.State.MarkAsCompleted()
+	p.state.MarkAsCompleted()
 }
 
 func (p *StepContext) RaiseTopicProvisionedEvent() error {
 	event := TopicProvisioned{
-		CapabilityRootId: string(p.State.CapabilityRootId),
-		ClusterId:        string(p.State.ClusterId),
-		TopicName:        p.State.TopicName,
+		CapabilityRootId: string(p.state.CapabilityRootId),
+		ClusterId:        string(p.state.ClusterId),
+		TopicName:        p.state.TopicName,
 	}
-	return p.Outbox.Produce(event)
+	return p.outbox.Produce(event)
 }
