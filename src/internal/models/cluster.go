d33b788849d0837151d764e08e98d735d113ebba
Thomas Wittrup Fischer <thofisch@gmail.com>
2023-03-09 21:33:33 +0100

    add schema registry api info

--- src/internal/models/cluster.go
+++ src/internal/models/cluster.go
@@ -3,11 +3,13 @@ package models
 type ClusterId string
 
 type Cluster struct {
-	ClusterId         ClusterId `gorm:"column:id;primarykey"`
-	Name              string
-	AdminApiEndpoint  string
-	AdminApiKey       ApiKey `gorm:"embedded;embeddedPrefix:admin_api_key_"`
-	BootstrapEndpoint string
+	ClusterId                 ClusterId `gorm:"column:id;primarykey"`
+	Name                      string
+	AdminApiEndpoint          string
+	AdminApiKey               ApiKey `gorm:"embedded;embeddedPrefix:admin_api_key_"`
+	BootstrapEndpoint         string
+	SchemaRegistryApiEndpoint string
+	SchemaRegistryApiKey      ApiKey `gorm:"embedded;embeddedPrefix:schema_registry_api_key_"`
 }
 
 func (*Cluster) TableName() string {
